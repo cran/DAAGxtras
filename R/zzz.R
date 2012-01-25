e2a7decadeaa8bbef63d6cde5afd8a6b3acc5e92
John Maindonald <john.maindonald@anu.edu.au>
2012-01-25 00:00:00 +0000

    version 0.8-3

--- R/zzz.R
+++
@@ -1,8 +0,0 @@
-.First.lib <-
-function (...)  {
-  cat("\nA number of datasets that were formerly in the",
-      "\nDAAGxtras package are now in the DAAG package:\n")
-  cat("\nAdditionally, the function dataFile() has been removed from DAAGxtras")
-  cat("\n(Use datafile(), in the DAAG package.)\n")
-}
-
